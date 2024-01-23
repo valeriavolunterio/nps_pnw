c5209f5e100ee24e4602cdb3b6ff1abf12db34ba
Kaylee_Best <Kaylee_Best@yahoo.com>
2024-01-22 22:12:42 -0500

    Updated Park Screen component

--- screens/subScreens/ParkScreen.js
+++ screens/subScreens/ParkScreen.js
@@ -3,11 +3,46 @@ import { SafeAreaView, Text } from "react-native";
 import { Button, ButtonGroup } from "@rneui/themed";
 // import { styles } from "../../App";
 
-const ParkScreen = ({ route, navigation }) => {
+const ParkScreen = () => {
+  // Example park data
+  const parkData = {
+    title: 'Example Park',
+    information: 'This is a beautiful park with lots of amenities.',
+    rotatingData: ['Event 1', 'Event 2', 'Event 3'], // Array of rotating data
+  };
+
+  const [rotatingIndex, setRotatingIndex] = useState(0);
+
+  // Use useEffect to rotate data every few seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRotatingIndex((prevIndex) =>
+        prevIndex === parkData.rotatingData.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [parkData.rotatingData]);
+
+  return (
+    <div>
+      <h2>{parkData.title}</h2>
+      <p>{parkData.information}</p>
+      <p>Rotating Data: {parkData.rotatingData[rotatingIndex]}</p>
+    </div>
+  );
+};
+
+export default ParkScreen;
+
+
+
+
+/* const ParkScreen = ({ route, navigation }) => {
   return (
     <SafeAreaView>
       <Text>ParkScreen</Text>
     </SafeAreaView>
   );
 };
-export default ParkScreen;
+export default ParkScreen; */
