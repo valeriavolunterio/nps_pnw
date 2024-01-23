7c310fd56b379276da6d866bd6d3dc9577858929
kharidubard <kharidubard@gmail.com>
2024-01-23 01:05:44 -0500

    Home Page Layout (in-progress)
    Added space for the carousel. Added the buttons for the home screen.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, SafeAreaView, Button, View, Text } from "react-native";
 import { Colors } from "../styles/Colors.js";
+import { Ionicons } from "@expo/vector-icons";
+import Svg, { Polygon } from "react-native-svg";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const styles = StyleSheet.create({
   container: {
@@ -33,6 +36,46 @@ const styles = StyleSheet.create({
     color: Colors.red,
     fontSize: 16,
   },
+
+  carouselContainer: {
+    padding: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 1,
+  },
+  carouselText: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  safetyGuideButton: {
+    alignItems: "stretch",
+    alignSelf: "center",
+    backgroundColor: Colors.baseTeal, 
+    padding: 20,
+    borderRadius: 5,
+    marginBottom: 20,
+    width: "90%",
+  },
+  safetyGuideButtonText: {
+    color: 'white',
+    fontSize: 16,
+    textAlign: "center"
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginVertical: 20,
+    marginHorizontal: 20,
+  },
+  iconContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
 });
 
 const HomeScreen = ({ route, navigation }) => {
@@ -44,33 +87,101 @@ const HomeScreen = ({ route, navigation }) => {
       <View style={styles.subHeaderContainer}>
         <Text style={styles.subHeaderText}>National Park Service</Text>
       </View>
-      <Button
-        title="Go to Park Screen"
-        onPress={() => navigation.navigate("Park")}
-      />
-      <Button
-        title="Go to Safety Guide Screen"
-        onPress={() => navigation.navigate("SafetyGuide")}
-      />
-      <Button
-        // Favorites, Bookmarks, Recents
-        title="Go to Saves Screen"
-        onPress={() => navigation.navigate("Saves")}
-      />
-      <Button
-        title="Go to Alerts Screen"
-        onPress={() => navigation.navigate("Alerts")}
-      />
-      <Button
-        title="Go to News Screen"
-        onPress={() => navigation.navigate("News")}
-      />
-      <Button
-        title="Go to Events Screen"
-        onPress={() => navigation.navigate("Events")}
-      />
 
+      <View style={styles.carouselContainer}>
+        <Text style={styles.carouselText}>Carousel</Text>
+      </View>
+
+      <TouchableOpacity
+      style={styles.safetyGuideButton}
+        onPress={() => navigation.navigate("SafetyGuide")} >
+          <Ionicons name="ios-glasses" size={24} color={Colors.darkTeal} style={styles.icon} />
+          <Text style={styles.safetyGuideButtonText}>Pacific Northwest Safety Guide</Text>
+      </TouchableOpacity>
+
+      <View style={styles.row}>
+        <View style={styles.iconContainer}>
+          <Ionicons
+            name="ios-star"
+            size={42}
+            color={Colors.darkTeal}
+            onPress={() => navigation.navigate("Saves")}
+          />
+        </View>
+
+        <View style={styles.iconContainer}>
+          <Ionicons
+            name="ios-bookmark"
+            size={42}
+            color={Colors.darkTeal}
+            onPress={() => navigation.navigate("Saves")}
+          />
+        </View>
+        <View style={styles.iconContainer}>
+          <Ionicons
+            name="ios-time"
+            size={42}
+            color={Colors.darkTeal}
+            onPress={() => navigation.navigate("Saves")}
+          />
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.iconContainer}>
+          <Ionicons
+            name="ios-warning"
+            size={42}
+            color={Colors.darkTeal}
+            onPress={() => navigation.navigate("Alerts")}
+          />
+        </View>
+        <View style={styles.iconContainer}>
+          <Ionicons
+            name="ios-newspaper"
+            size={42}
+            color={Colors.darkTeal}
+            onPress={() => navigation.navigate("News")}
+          />
+        </View>
+        <View style={styles.iconContainer}>
+          <Ionicons
+            name="ios-calendar"
+            size={42}
+            color={Colors.darkTeal}
+            onPress={() => navigation.navigate("Events")}
+          />
+        </View>
+      </View>
     </SafeAreaView>
+
+    //Old buttons to different screens
+    /* 
+        <Button
+          title="Go to Park Screen"
+          onPress={() => navigation.navigate("Park")}
+        />
+        <Button
+          title="Go to Safety Guide Screen"
+          onPress={() => navigation.navigate("SafetyGuide")}
+        />
+        
+        <Button
+          // Favorites, Bookmarks, Recents
+          title="Go to Saves Screen"
+          onPress={() => navigation.navigate("Saves")}
+        />
+        <Button
+          title="Go to Alerts Screen"
+          onPress={() => navigation.navigate("Alerts")}
+        />
+        <Button
+          title="Go to News Screen"
+          onPress={() => navigation.navigate("News")}
+        />
+        <Button
+          title="Go to Events Screen"
+          onPress={() => navigation.navigate("Events")}
+        />*/
   );
 };
 
