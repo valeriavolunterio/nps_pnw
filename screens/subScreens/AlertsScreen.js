700b0b497d1a39e42e3b18f5742157e16d61dfd0
kaedinscott12 <kaedin.scott@gmail.com>
2024-01-26 19:51:14 -0500

    alert screen styled

--- screens/subScreens/AlertsScreen.js
+++ screens/subScreens/AlertsScreen.js
@@ -8,20 +8,58 @@ import {
   Pressable,
   FlatList,
 } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 // import { styles } from "../../App";
 
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
+    marginLeft: 23,
+    flex: 1,
+    overflow: "hidden",
   },
   label: {
     marginLeft: 10,
   },
   horizontalRule: {
     borderBottomColor: "black", // Change color as needed
+    opacity: 0.2,
     borderBottomWidth: 1,
-    marginVertical: 10,
+    marginVertical: 6,
     width: "100%", // Adjust width as needed
+  },
+  heading: {
+    fontWeight: 600,
+    fontSize: 16,
+    marginTop: 20,
+    color: "#616E73",
+  },
+  subHeading: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 55,
+    marginLeft: 8,
+    paddingBottom: 10,
+  },
+  body: {
+    fontSize: 12,
+    alignItems: "center",
+    marginLeft: 8,
+    paddingBottom: 13,
+    marginRight: 55,
+  },
+  alertContainer: {
+    flexDirection: "row",
+    paddingBottom: 5,
+    //flex: 1,
+  },
+
+  iconContainer: {
+    //marginRight: 10,
+    //flex: 1,
+  },
+  textContainer: {
+    //flex: 1,
   },
 });
 
@@ -55,9 +93,29 @@ const activeAlerts = [
       "Hurricane Ridge Road closed for demolition and preparation for winter",
     body: "Beginning on October 16, demolition and removal of the remaining Hurricane Ridge Day Lodge debris begins. This critical step in the ongoing efforts to restore visitor services and ensure public safety will require a temporary closure of the area.",
   },
+  {
+    id: 5,
+    park: "San Juan",
+    type: "Danger",
+    heading: "Burn Ban in Effect",
+    body: "The superintendent has issued a ban on outdoor and recreational fires due atmospheric conditions that have resulted in an increased risk of wildfire. This ban will remain in effect until further notice.",
+  },
+  {
+    id: 6,
+    park: "San Juan",
+    type: "Caution",
+    heading: "Wildlife Viewing Guidelines",
+    body: "San Juan Island National Historical Park provides habitat for many species of animals. When you visit the park, you are visiting their home. Do not approach any wildlife within 75 ft/23 m (two school bus lengths).",
+  },
 ];
 
 const AlertsList = ({ alerts }) => {
+  const iconMapping = {
+    Closure: { name: "ios-remove-circle", color: "rgba(186, 52, 48, 1)" },
+    Info: { name: "ios-information-circle", color: "rgba(48, 80, 163, 1)" },
+    Danger: { name: "ios-warning", color: "rgba(104, 26, 14, 1)" },
+    Caution: { name: "ios-warning", color: "rgba(224, 137, 73, 1)" },
+  };
   // Group alerts by park
   const alertsByPark = alerts.reduce((acc, alert) => {
     if (!acc[alert.park]) {
@@ -68,18 +126,30 @@ const AlertsList = ({ alerts }) => {
   }, {});
 
   const renderAlertItem = ({ item }) => (
-    <View>
-      {/* display icon according to type */}
-      <Text>{item.heading}</Text>
-      <Text numberOfLines={2} ellipsizeMode="tail">
-        {item.body}
-      </Text>
+    <View styles={styles.container}>
+      <View style={styles.alertContainer}>
+        {/* display icon according to type */}
+        <View style={styles.iconContainer}>
+          <Ionicons
+            name={iconMapping[item.type].name}
+            size={30}
+            color={iconMapping[item.type].color}
+          />
+        </View>
+        {/*Text content on the right of icon */}
+        <View style={styles.textContainer}>
+          <Text style={styles.subHeading}>{item.heading}</Text>
+          <Text style={styles.body} numberOfLines={3} ellipsizeMode="tail">
+            {item.body}
+          </Text>
+        </View>
+      </View>
     </View>
   );
 
   const renderParkAlerts = ({ item: park }) => (
     <View>
-      <Text>{park} National Park</Text>
+      <Text style={styles.heading}>{park} National Park</Text>
       <View style={styles.horizontalRule} />
       <FlatList
         data={alertsByPark[park]}
@@ -105,4 +175,5 @@ const AlertsScreen = ({ route, navigation }) => {
     </SafeAreaView>
   );
 };
+
 export default AlertsScreen;
