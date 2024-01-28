64d43fbbb7b0cdbb08e8acfc109e8ee15f77b2ce
kaedinscott12 <kaedin.scott@gmail.com>
2024-01-27 22:31:00 -0500

    continuation of styling SafteyGuide

--- screens/subScreens/SafetyGuideScreen.js
+++ screens/subScreens/SafetyGuideScreen.js
@@ -27,15 +27,49 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginLeft: 60,
     fontSize: 16,
+    ...Fonts.subheading,
   },
   subHeading: {
     margin: 20,
+
+    fontWeight: "bold",
+    fontSize: 14,
   },
   beigeBackground: {
     flex: 1,
     backgroundColor: "#FFF9F5", // Beige color
     overflow: "hidden", // Clip overflow content
     width: "100%",
+  },
+  listItemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  listHeading: {
+    marginBottom: 2,
+    fontWeight: 400,
+    fontSize: 18,
+    ...Fonts.header3,
+  },
+  listSubHeading: {
+    marginRight: 130,
+    marginBottom: 18,
+    fontWeight: 700,
+    //...Fonts.body,
+    fontStyle: "italic",
+  },
+  listItemText: {
+    ...Fonts.body,
+    // marginTop: 20,
+    // marginLeft: 20,
+    marginBottom: -3,
+    //margin: 10,
+  },
+  readMore: {
+    flexDirection: "row-reverse",
+    ...Fonts.subheading,
+    ...Colors.darkGray,
   },
   svgTriangle: {
     flex: 1,
@@ -47,13 +81,6 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
   },
 
-  bodyText: {
-    ...Fonts.body,
-    // marginTop: 20,
-    // marginLeft: 20,
-    // marginBottom: 2
-    //margin: 10,
-  },
   triangleContainer: {
     alignItems: "center",
     borderRadius: 10,
@@ -97,73 +124,125 @@ const SafetyGuideScreen = ({ route, navigation }) => {
         </Svg>
       </View>
       <View style={styles.cardContainer}>
-        <Text>Tides and Your Safety</Text>
-        <Text style={styles.bodyText}>
-          Always carry a tide table, topographic map, and keep track of the time
-          whenever hiking along Olympic's coast
+        <Text style={styles.listHeading}>Tides and Your Safety</Text>
+        <Text style={styles.listSubHeading}>
+          <Text style={{ fontWeight: "bold" }}>Always</Text> carry a tide table,
+          topographic map, and keep track of the time whenever hiking along
+          Olympic's coast
         </Text>
-        <Text style={styles.bodyText}>
-          Several points along the coast are only passable at lower tides. Check
-          the NOAA Tide Predictions to see if your chosen days are appropriate
-          for coastal travel.
-        </Text>
-        <Text style={styles.bodyText}>
-          Always carry a tide chart, available at visitor centers and coastal
-          ranger stations, to time your hikes accordingly.
-        </Text>
-        <Text style={styles.bodyText}>
-          When hiking the coast, you will need a topographic map that shows you
-          the headlands that are only passable at lower tides. This map along
-          with your tide table are essential to safely enjoying this rugged
-          wilderness. Maps can be purchased ahead of time online here, at some
-          local gear shops, or in person at select visitor centers
-        </Text>
-        <Pressable>
-          <Text>Read More</Text>
-        </Pressable>
+
+        <View style={styles.listItemContainer}>
+          {/* <Text style={{ fontSize: 20, marginBottom: 55 }}>{"\u2022"}</Text> */}
+          <Text style={styles.listItemText}>
+            {"\u2022"}Several points along the coast are only passable at lower
+            tides. Check the NOAA Tide Predictions to see if your chosen days
+            are appropriate for coastal travel.{"\n"}
+          </Text>
+        </View>
+        <View style={styles.listItemContainer}>
+          <Text style={styles.listItemText}>
+            {"\u2022"}Always carry a tide chart, available at visitor centers
+            and coastal ranger stations, to time your hikes accordingly.{"\n"}{" "}
+          </Text>
+        </View>
+        <View style={styles.listItemContainer}>
+          <Text style={styles.listItemText}>
+            {"\u2022"}When hiking the coast, you will need a topographic map
+            that shows you the headlands that are only passable at lower tides.
+            This map along with your tide table are essential to safely enjoying
+            this rugged wilderness. Maps can be purchased ahead of time online
+            here, at some local gear shops, or in person at select visitor
+            centers{"\n"}
+          </Text>
+        </View>
+        <View style={styles.readMore}>
+          <Pressable>
+            <Text>Read More</Text>
+          </Pressable>
+        </View>
       </View>
       <View style={styles.cardContainer}>
-        <Text>Wildlife Safety</Text>
-        <Text style={styles.bodyText}>
-          All park wildlife are potentially dangerous to humans and can
-          sometimes be unpredictable.
+        <Text style={styles.listHeading}>Wildlife Safety</Text>
+        <Text style={styles.listSubHeading}>
+          <Text style={{ fontWeight: "bold" }}>
+            All park wildlife are potentially dangerous to humans and can
+            sometimes be unpredictable.
+          </Text>
         </Text>
-        <Text style={styles.bodyText}>
-          Here are some basic guidelines that may help lessen the threat of
-          danger and will help to keep wildlife wild:
-        </Text>
-        <Text style={styles.bodyText}>
-          Observe wildlife from a distance. All wildlife is protected in the
-          park. Park regulations require that all visitors maintain a distance
-          of at least 50 yards (half the length of a football field) between
-          themselves and any park wildlife. Do not approach wildlife. If an
-          animal approaches closer than 50 yards, move away to maintain the
-          minimum required distance of separation.
-        </Text>
-        <Text style={styles.bodyText}>
-          Never feed wild animals. This includes all park animals: birds,
-          squirrels, marmots, deer, elk, otters etc., not just bears. Learning
-          to beg for and/or rely on human foods is extremely harmful for all
-          wild animals, big or small. Be careful not to leave wrappers, crumbs,
-          or other food trash after picnicking or snacking on the trails.
-          Feeding wildlife damages their health, alters natural behaviors, and
-          exposes them to predators and other dangers. Feeding wildlife can also
-          increase risks to you and other visitors.
-        </Text>
-        <Text style={styles.bodyText}>
-          Avoid wildlife during sensitive times.This includes times when animals
-          are mating, nesting, raising young, and during the winter.
-        </Text>
-        <Pressable>
-          <Text>Read More</Text>
-        </Pressable>
+        <View style={styles.listItemContainer}>
+          {/* <Text style={{ fontSize: 20, marginBottom: 55 }}>{"\u2022"}</Text> */}
+          <Text style={styles.listItemText}>
+            {"\u2022"}Observe wildlife from a distance. All wildlife is
+            protected in the park. Park regulations require that all visitors
+            maintain a distance of at least 50 yards (half the length of a
+            football field) between themselves and any park wildlife. Do not
+            approach wildlife. If an animal approaches closer than 50 yards,
+            move away to maintain the minimum required distance of separation.
+            {"\n"}
+          </Text>
+        </View>
+        <View style={styles.listItemContainer}>
+          <Text style={styles.listItemText}>
+            {"\u2022"}Never feed wild animals. This includes all park animals:
+            birds, squirrels, marmots, deer, elk, otters etc., not just bears.
+            Learning to beg for and/or rely on human foods is extremely harmful
+            for all wild animals, big or small. Be careful not to leave
+            wrappers, crumbs, or other food trash after picnicking or snacking
+            on the trails. Feeding wildlife damages their health, alters natural
+            behaviors, and exposes them to predators and other dangers. Feeding
+            wildlife can also increase risks to you and other visitors.{"\n"}{" "}
+          </Text>
+        </View>
+        <View style={styles.listItemContainer}>
+          <Text style={styles.listItemText}>
+            {"\u2022"}Avoid wildlife during sensitive times.This includes times
+            when animals are mating, nesting, raising young, and during the
+            winter.{"\n"}
+          </Text>
+        </View>
+        <View style={styles.readMore}>
+          <Pressable>
+            <Text>Read More</Text>
+          </Pressable>
+        </View>
       </View>
+      <View style={styles.cardContainer}>
+        <Text style={styles.listHeading}>Bears</Text>
+        <Text style={styles.listSubHeading}>
+          <Text>
+            Minimize bear encounters: keep a clean camp and store food properly.
+          </Text>
+        </Text>
+
+        <View style={styles.listItemContainer}>
+          {/* <Text style={{ fontSize: 20, marginBottom: 55 }}>{"\u2022"}</Text> */}
+        </View>
+        <View style={styles.listItemContainer}>
+          <Text style={styles.listItemText}>
+            There have been several instances of aggressive bears in the
+            Olympics. No injuries have been reported, but property was damaged
+            and bears have acted in a threatening manner. If you meet a bear on
+            the trail, give it a wide berth. If a bear comes into camp, make
+            noise to scare the bear away. If it is intent on getting your food
+            or other property, do not risk injury. In the face of repeated
+            encounters, leave the area, with or without your property as
+            appropriate. Notify park staff in all instances of food loss or
+            property damage, or any other threatening acts by bears.{"\n"}
+          </Text>
+        </View>
+        <View style={styles.readMore}>
+          <Pressable>
+            <Text>Read More</Text>
+          </Pressable>
+        </View>
+      </View>
+      {/* 
       <View style={styles.cardContainer}>
         <Text>Bears</Text>
-        <Text style={styles.bodyText}>
+        <Text style={styles.listItemText}>
           Minimize bear encounters: keep a clean camp and store food properly.
         </Text>
-        <Text style={styles.bodyText}>
+        <Text style={styles.listItemText}>
           There have been several instances of aggressive bears in the Olympics.
           No injuries have been reported, but property was damaged and bears
           have acted in a threatening manner. If you meet a bear on the trail,
@@ -177,7 +256,7 @@ const SafetyGuideScreen = ({ route, navigation }) => {
         <Pressable>
           <Text>Read More</Text>
         </Pressable>
-      </View>
+      </View> */}
     </ScrollView>
   );
 };
